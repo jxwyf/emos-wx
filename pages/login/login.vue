e5d67c50ebbfe6ef719abdf43d2eeab70de66197
2578831517@qq.com <2578831517@qq.com>
2021-08-28 18:00:53 +0800

    登录页面以及注册页面完成 获取微信账号信息

---
+++ pages/login/login.vue
@@ -0,0 +1,34 @@
+<template>
+	<view>
+		<image src="../../static/logo-1.png" class="logo" mode="widthFix"></image>
+		<view class="logo-title">企业办公管理系统</view>
+		<view class="logo-subtitle">Ver 0.0.1</view>
+		<!-- open-type="getUserInfo" 获取微信用户信息 -->
+		<button class="login-btn" open-type="getUserInfo" @tap="login()">登录系统</button>
+		<view class="register-container">
+			没有账号
+			<text class="register" @tap="toRegister()">立即注册</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			toRegister:function(){
+				uni.navigateTo({
+					url:"../register/register"
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="less">
+	@import url("login.less");
+</style>
---
+++ pages/register/register.vue
@@ -0,0 +1,44 @@
+<template>
+	<view>
+		<image src="../../static/logo-2.png" mode="widthFix" class="logo"></image>
+		<view class="register-container">
+			<input type="text" placeholder="输入你的邀请码" class="register-code" maxlength="6" v-model="registerCode" />
+			<view class="register-desc">管理员创建员工证账号之后 你可以从你的个人邮箱中获得注册邀请码</view>
+			<button class="register-btn" open-type="getUserInfo" @tap="register()">执行注册</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			register:function(){
+				uni.login({
+					provider:"weixin",
+					success:function(resp){
+						let code = resp.code;
+						// 获取微信账号基本信息
+						uni.getUserInfo({
+							provider:"weixin",
+							success:function(resp){
+								//用户昵称
+								let nickName = resp.userInfo.nickName;
+								//用户头像url
+								let acatarUrl = resp.userInfo.avatarUrl;
+							}
+						})
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="less">
+	@import url("register.less");
+</style>
